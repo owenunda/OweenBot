2525208f20be6fb7333ceff5ba88ddda0a89a1e1
owenunda <oweenunda.dev@gmail.com>
2023-10-03 01:25:32 -0500

    new command echo

---
+++ commands/fun/echo.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('echo')
+	.setDescription('Replies with your input!')
+	.addStringOption((option) =>
+      option
+        .setName("input")
+        .setDescription("Replies with your input!")
+        .setRequired(true)),
+
+        async execute(interaction){
+            const input = await interaction.options.getString("input");
+            await interaction.reply(`Dijiste: ${input}`)
+        },
+}
