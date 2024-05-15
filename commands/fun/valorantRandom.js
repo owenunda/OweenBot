3373fa02238622e11fd0dde47530966ca02a2ba1
owenunda <oweenunda.dev@gmail.com>
2024-05-14 20:44:38 -0500

    [improvement] random-agent

--- commands/fun/valorantRandom.js
+++ commands/fun/valorantRandom.js
@@ -1,29 +1,24 @@
 const {SlashCommandBuilder, EmbedBuilder} = require("discord.js")
 const agentValorant = require("./json/agentValorant.json")
 
+let imgAgent = ''
+let nameAgent = ''
+
 const randomAgent = () =>{
-  let numRandom = Math.floor(Math.random() * (agentValorant.length - 1)) 
-  return agentValorant[numRandom].img
+  const numRandom = Math.floor(Math.random() * (agentValorant.length - 1)) 
+  imgAgent = agentValorant[numRandom].img
+  nameAgent = agentValorant[numRandom].name
 }
+
+randomAgent()
+
 
 const exampleEmbed = new EmbedBuilder()
 	.setColor(0x0099FF)
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setAuthor({ name: `The random agent is ${nameAgent} 😉 `, iconURL: 'https://cdn4.iconfinder.com/data/icons/valorant-jett-and-killjoy-cute-chibi/2000/valorant_chibi_killjoy_jettvalorant_cute_jettcute_killjoycute-09-512.png'})
+	.setImage(imgAgent)
 	.setTimestamp()
-	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
+  .setFooter({ text: 'thanks for using oweenbot ❤️', iconURL: 'https://www.techspot.com/images2/downloads/topdownload/2020/06/2020-06-09-ts3_thumbs-7fd-p_256.webp' });
 
 module.exports = {
     cooldown: 5,
@@ -33,6 +28,6 @@ module.exports = {
     
     async execute(interaction){
       
-        await interaction.reply(randomAgent())
+        await interaction.reply({ embeds: [exampleEmbed] })
     }
 }
